c9997fd020d8080ccbb41b1cf77276f0a27f1c8c
Evan Suma Rosenberg <suma@umn.edu>
2023-03-16 17:01:52 -0500

    class update

--- src/RobotPart.ts
+++ src/RobotPart.ts
@@ -58,6 +58,17 @@ export class RobotPart extends gfx.Transform3
             this.boneLength = 0.4;
             this.boneDirection.set(0.001, 1, 0);
             this.boneDirection.normalize();
+
+            const child = new RobotPart('endEffector');
+            child.position.copy(this.boneDirection);
+            child.position.multiplyScalar(this.boneLength);
+            this.add(child);
+        }
+        else if(this.name == 'endEffector')
+        {
+            this.boneLength = 0.075;
+            this.boneDirection.set(0.001, 1, 0);
+            this.boneDirection.normalize();
         }
     }
 
@@ -65,10 +76,10 @@ export class RobotPart extends gfx.Transform3
     // Each mesh will be defined in the robot part's local space.
     createMeshes(): void
     {
-        const axes = new gfx.Axes3(0.15);
-        axes.lookAt(this.boneDirection);
-        axes.translateZ(-this.boneLength);
-        this.add(axes);
+        // const axes = new gfx.Axes3(0.15);
+        // axes.lookAt(this.boneDirection);
+        // axes.translateZ(-this.boneLength);
+        // this.add(axes);
 
         if(this.name == 'upperArm')
         {
@@ -105,6 +116,37 @@ export class RobotPart extends gfx.Transform3
             sphere.lookAt(this.boneDirection);
             sphere.translateZ(-this.boneLength);
             this.add(sphere);
+        }
+        else if(this.name == 'endEffector')
+        {
+            const pincherRoot = new gfx.Transform3();
+            pincherRoot.lookAt(this.boneDirection);
+            pincherRoot.translateZ(-this.boneLength/2);
+            this.add(pincherRoot);
+
+            const pincher = gfx.MeshFactory.createBox(0.025, 0.025, 0.1);
+
+            const leftPincher1 = new gfx.MeshInstance(pincher);
+            leftPincher1.translateX(0.05);
+            leftPincher1.rotateY(gfx.MathUtils.degreesToRadians(-45));
+            pincherRoot.add(leftPincher1);
+
+            const leftPincher2 = new gfx.MeshInstance(pincher);
+            leftPincher2.translateZ(-0.04);
+            leftPincher2.rotateY(gfx.MathUtils.degreesToRadians(75));
+            leftPincher2.translateZ(-0.04);
+            leftPincher1.add(leftPincher2);
+
+            const rightPincher1 = new gfx.MeshInstance(pincher);
+            rightPincher1.translateX(-0.05);
+            rightPincher1.rotateY(gfx.MathUtils.degreesToRadians(45));
+            pincherRoot.add(rightPincher1);
+
+            const rightPincher2 = new gfx.MeshInstance(pincher);
+            rightPincher2.translateZ(-0.04);
+            rightPincher2.rotateY(gfx.MathUtils.degreesToRadians(-75));
+            rightPincher2.translateZ(-0.04);
+            rightPincher1.add(rightPincher2);
         }
 
         this.children.forEach((child: gfx.Transform3)=>{
