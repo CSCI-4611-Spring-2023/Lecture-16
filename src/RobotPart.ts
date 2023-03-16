e2c516e576b75d943bf6e6d40be3dd78b65fb73a
Evan Suma Rosenberg <suma@umn.edu>
2023-03-16 16:48:37 -0500

    class update

--- src/RobotPart.ts
+++ src/RobotPart.ts
@@ -23,7 +23,7 @@ export class RobotPart extends gfx.Transform3
         if(this.name == 'root')
         {
             this.boneLength = 0.05;
-            this.boneDirection.set(0, 1, 0);
+            this.boneDirection.set(0.001, 1, 0);
             this.boneDirection.normalize();
 
             const child = new RobotPart('upperArm');
@@ -34,7 +34,7 @@ export class RobotPart extends gfx.Transform3
         else if(this.name == 'upperArm')
         {
             this.boneLength = 0.5;
-            this.boneDirection.set(0, 1, 0);
+            this.boneDirection.set(0.001, 1, 0);
             this.boneDirection.normalize();
 
             const child = new RobotPart('middleArm');
@@ -44,7 +44,20 @@ export class RobotPart extends gfx.Transform3
         }
         else if(this.name == 'middleArm')
         {
-            //
+            this.boneLength = 0.4;
+            this.boneDirection.set(0.001, 1, 0);
+            this.boneDirection.normalize();
+
+            const child = new RobotPart('lowerArm');
+            child.position.copy(this.boneDirection);
+            child.position.multiplyScalar(this.boneLength);
+            this.add(child);
+        }
+        else if(this.name == 'lowerArm')
+        {
+            this.boneLength = 0.4;
+            this.boneDirection.set(0.001, 1, 0);
+            this.boneDirection.normalize();
         }
     }
 
@@ -53,15 +66,45 @@ export class RobotPart extends gfx.Transform3
     createMeshes(): void
     {
         const axes = new gfx.Axes3(0.15);
+        axes.lookAt(this.boneDirection);
+        axes.translateZ(-this.boneLength);
         this.add(axes);
 
         if(this.name == 'upperArm')
         {
-            //
+            const arm = gfx.MeshFactory.createBox(0.05, 0.05, this.boneLength);
+            arm.lookAt(this.boneDirection);
+            arm.translateZ(-this.boneLength/2);
+            this.add(arm);
+
+            const sphere = gfx.MeshFactory.createSphere(0.05, 1);
+            sphere.lookAt(this.boneDirection);
+            sphere.translateZ(-this.boneLength);
+            this.add(sphere);
         }
         else if(this.name == 'middleArm')
         {
-            //
+            const arm = gfx.MeshFactory.createBox(0.05, 0.05, this.boneLength);
+            arm.lookAt(this.boneDirection);
+            arm.translateZ(-this.boneLength/2);
+            this.add(arm);
+
+            const sphere = gfx.MeshFactory.createSphere(0.05, 1);
+            sphere.lookAt(this.boneDirection);
+            sphere.translateZ(-this.boneLength);
+            this.add(sphere);
+        }
+        else if(this.name == 'lowerArm')
+        {
+            const arm = gfx.MeshFactory.createBox(0.05, 0.05, this.boneLength);
+            arm.lookAt(this.boneDirection);
+            arm.translateZ(-this.boneLength/2);
+            this.add(arm);
+
+            const sphere = gfx.MeshFactory.createSphere(0.05, 1);
+            sphere.lookAt(this.boneDirection);
+            sphere.translateZ(-this.boneLength);
+            this.add(sphere);
         }
 
         this.children.forEach((child: gfx.Transform3)=>{
